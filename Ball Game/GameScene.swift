e49925d46e09ea11714cf1107f37b72a2b148f33
CHRISTOPHER COOPER <christopher.cooper22@clcmbp10.local>
2019-05-02 12:43:54 -0500

    Added funds for cannon

--- Ball Game/GameScene.swift
+++ Ball Game/GameScene.swift
@@ -1,22 +1,28 @@
-//
-//  GameScene.swift
-//  Ball Game
-//
-//  Created by LUCAS KURKOWSKI on 5/2/19.
-//  Copyright © 2019 clc.kurkowski. All rights reserved.
-//
+
+
+
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+   // var Cannon = SKSpriteNode.self
     
     override func didMove(to view: SKView) {
         
-}
+    
+    }
+
+    func CannonShoot() {
+        
+        
+    }
+
+    func CannonMove()  {
+        
+    }
+
 
 
 
