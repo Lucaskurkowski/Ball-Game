816b8a4629c0f16123f48ad49372eab4d13784f6
LUCAS KURKOWSKI <lucas.kurkowski22@clcmbp11.local>
2019-05-07 13:07:46 -0500

    Adding physics delegate

--- Ball Game/GameViewController.swift
+++ Ball Game/GameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,SKPhysicsContactDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
